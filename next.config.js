f73dec24af35de65feae9df0b476b6ab580a72e6
titan-elite-commerce <yasserrr.fox17@gmail.com>
2025-11-28 08:04:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      { source: '/commerce', destination: '/', permanent: true },
+      { source: '/ecommerce', destination: '/', permanent: true },
+      { source: '/assets', destination: '/', permanent: true },
+      { source: '/analytics', destination: '/', permanent: true },
+      { source: '/fundx', destination: '/', permanent: true },
+      { source: '/estate', destination: '/', permanent: true },
+      { source: '/insure', destination: '/', permanent: true },
+      { source: '/dx', destination: '/', permanent: true },
+      { source: '/explorer', destination: '/', permanent: true },
+      { source: '/nbf', destination: '/', permanent: true },
+      { source: '/epic', destination: '/', permanent: true },
+      { source: '/legend', destination: '/', permanent: true },
+      { source: '/connection', destination: '/', permanent: true },
+      { source: '/system', destination: '/', permanent: true },
+      { source: '/alerts', destination: '/', permanent: true },
+      { source: '/nx', destination: '/', permanent: true },
+      { source: '/nexus', destination: '/', permanent: true },
+      { source: '/brookfield', destination: '/', permanent: true },
+      { source: '/sab', destination: '/', permanent: true },
+      { source: '/vip', destination: '/', permanent: true },
+      { source: '/titan', destination: '/', permanent: true },
+      { source: '/zone', destination: '/', permanent: true },
+      { source: '/elite', destination: '/', permanent: true },
+    ]
+  },
+}
 
+module.exports = nextConfig
