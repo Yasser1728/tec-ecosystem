16723446890021fcd928410303ce25c56f2c7654
titan-elite-commerce <yasserrr.fox17@gmail.com>
2025-12-24 13:26:31 +0200

    Refactor next.config.js for serverActions configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverActions: true
-  },
-  images: {
-    domains: ["ipfs.io", "res.cloudinary.com"]
-  },
-  compress: true
-};
-
-module.exports = nextConfig;
+    serverActions: {
+      enabled: true
+    }
+  }
+}
