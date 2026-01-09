e189e1d26cb91ec43a3cbdea4d121101a75433dc
titan-elite-commerce <yasserrr.fox17@gmail.com>
2026-01-09 17:05:12 +0200

    Change module.exports to export default in config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,9 @@
-module.exports = {
+// next-i18next.config.js
+export default {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ar"],
     localeDetection: false,
   },
-  reloadOnPrerender: process.env.NODE_ENV === "development",
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
